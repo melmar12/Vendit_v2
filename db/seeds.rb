4a0b84da24055988b69d70b9b6eab11fb1c08a42
Melissa Martinez <melmar12@live.com>
2017-06-15 18:31:11 -0500

    added db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: 'admin', password: '123', admin: true)
+
+Alert.create(message: 'school is closed today', show: true)
+Alert.create(message: 'dont come to school', show: true)
+Alert.create(message: 'school is back in session', show: false)
+
+Post.create(title: "hello", body: 'todays lunch is pizza')
+Post.create(title: "hello", body: 'school is closed thursday')
+Post.create(title: "hello", body: 'field day tomorrow')
+Post.create(title: "hello", body: 'dress up day tomorrow')
