5c35ea53df9383e9bb579f9df241be949b0f19dc
Melissa Martinez <melmar12@live.com>
2017-06-16 19:54:21 -0500

    corrected model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,11 +1,12 @@
 class Document < ActiveRecord::Base
   # This method associates the attribute ":avatar" with a file attachment
-has_attached_file :doc, styles: {
-  thumb: '100x100>',
-  square: '200x200#',
-  medium: '300x300>'
-}
+  validates :doc,
+    attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
+    attachment_size: { less_than: 5.megabytes }
 
-# Validate the attached image is image/jpg, image/png, etc
-validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z
+  has_attached_file :doc, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
 end
