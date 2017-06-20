593d025eed69867fdeb07d0128106f2ba5088c01
Melissa Martinez <melmar12@live.com>
2017-06-19 19:42:38 -0500

    login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,26 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :get_alerts
+  helper_method :current_user, :logged_in?
+
 
   def get_alerts
     @alerts = Alert.all
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+    flash[:danger] = "you must be logged in to perform that action"
+    redirect_to root_path
+    end
   end
 
 end
