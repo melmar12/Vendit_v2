897846037b2f7944fcb0bab330d3a297db05ee82
Melissa Martinez <melmar12@live.com>
2017-06-22 04:05:20 -0500

    merp

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < ActiveRecord::Base
   # This method associates the attribute ":avatar" with a file attachment
   validates :doc,
     # attachment_content_type: {content_type: /\Aimage\/.*\Z/},
-    attachment_size: { less_than: 5.megabytes }
+    attachment_size: { less_than: 8.megabytes }
 
   has_attached_file :doc, styles: {
     # thumb: '100x100>',
